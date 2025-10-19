ea90f6cc13057569e9155bfdf669080c0d03938c
arielwho <daimlercoupe1122@gmail.com>
2025-10-19 13:44:06 +0200

    Add CLI argument parsing with argparse

--- join.py
+++ join.py
@@ -1,6 +1,14 @@
+import os 
+import argparse
+
 def join_files(file_list, output_file):
-    #TODO: implement join logic
-    pass
+    with open(file_list, "w", encoding='utf-8')  as f:
+        lines = f.readlines()
+
 
 if __name__ == "__main__":
-    print("Join CLI tool - still in progress...")
+    parser = argparse.ArgumentParser(description="Join CLI tool")
+    parser.add_argument("-f" ,"--file", type=str, nargs="+", help="List of txt files to combine")
+    parser.add_argument("-o", "--output", default="combined.txt", help="Name of the output file.")
+    parser.add_argument("-v", "--verbose", action = "store_true", help="Show detailed output")
+    args = parser.parse_args()      #pristupam kao args.files; args.output; args.verbose
