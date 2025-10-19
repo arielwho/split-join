9ce75936a858cb3cf59e07732e7d121cd7a8b173
arielwho <daimlercoupe1122@gmail.com>
2025-10-20 01:30:44 +0200

    Fixed variable name in join.py loop\

--- join.py
+++ join.py
@@ -7,18 +7,19 @@ def join_files(files, output, verbose):
     with open(output, "w", encoding='utf-8')  as outfile:
         for file in files:
             if os.path.exists(file):
-                with open(files, "r", encoding="utf-8") as infile:
+                with open(file, "r", encoding="utf-8") as infile:
                     lines = infile.readlines()
                     total_lines += len(lines)
                     outfile.writelines(lines)
                     outfile.write("\n")
+                if verbose:
+                    print(f"Added {len(lines)} lines from {file}")
             else:
                 print("Selected file does not exist.")
 
-                if verbose:
-                    print(f"Added {len(lines)} from {file}")
+                
     
-    print(f"Created {outfile} with total {total_lines} lines.")
+    print(f"Created '{output}' with total {total_lines} lines.")
 
 
 if __name__ == "__main__":
@@ -27,3 +28,8 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", default="combined.txt", help="Name of the output file.")
     parser.add_argument("-v", "--verbose", action = "store_true", help="Show detailed output")
     args = parser.parse_args()      #pristupam kao args.files; args.output; args.verbose
+
+    if not args.file:
+        print("No input files provided. Use -f to specify files.")
+    else:
+        join_files(args.file, args.output, args.verbose)
